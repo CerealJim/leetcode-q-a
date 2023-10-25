a91976c8cde2725a13d7d38d75b9e8f382542032
CerealJim <jim.haz34@gmail.com>
2023-10-25 10:06:51 -0400

    capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let arr = str.split(" ");
+  let newStr = "";
+
+  for (let word of arr) {
+    let letter = word.split("");
+    let cap = letter[0].toUpperCase();
+
+    letter.shift();
+    letter.unshift(cap);
+
+    newStr += letter.join("") + " ";
+  }
+
+  return newStr.trim();
+}
 
 module.exports = capitalize;
